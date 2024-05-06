84348dca221708d42afecb3dffa52f9b4b5acfca
Oren Storms <orenstorms@icloud.com>
2024-05-06 13:23:38 -0500

    changed the format of the coding standards and renamed some functions

--- functionInits.h
+++ functionInits.h
@@ -1,3 +1,6 @@
-int bubble_sort_int(int* intArray, int sizeOfArray);
-int sort_int_array(int* intArray, int sizeOfArray);
-int quick_sort_int(int arr[], int low, int high);
+int sortArray_int(int intArray[], int sizeOfArray);
+void swapMem(int* Element1, int* Element2);
+int bubbleSort_int(int intArray[], int sizeOfArray);
+int quickSortPartition_int(int arr[], int low, int high); 
+int quickSort_int(int arr[], int low, int high);
+int gcd(int element1, int element2);
---
+++ functions.h
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "functionInits.h"
+
+//global constants
+#define pi      (atan(1))
+#define e       2.718281828459045
+#define rt2     sqrt(2)
+#define rt3     sqrt(3)
+
+
+
+int sortArray_int(int intArray[], int sizeOfArray)
+{
+    quickSort_int(intArray, 0, sizeOfArray - 1);
+    //bubbleSort_int(intArray, sizeOfArray);
+    
+    return 0;
+}
+
+void swapMem(int* Element1, int* Element2)
+{
+    int swapRegister;
+
+    swapRegister = *Element1;
+    *Element1 = *Element2;
+    *Element2 = swapRegister;
+}
+
+int bubbleSort_int(int intArray[], int sizeOfArray)
+{
+    int i, j;
+
+    //going through the array as many times as there are elements
+    for(i = 0; i < sizeOfArray - 1; i++)
+    {
+        //starting at the beginning and checking each element until you 
+        //reach the end of the unsorted portion of the array
+        for(j = 0; j < sizeOfArray - 1 - i; j++)
+        {
+            //checking if the current element needs to be swapped with it's neighbor
+            //and doing so if needed
+            if(intArray[j] > intArray[j + 1])
+            {
+                swapMem(&intArray[j], &intArray[j + 1]);
+            }
+        }
+    }
+
+    return 0;
+}
+
+int quickSortPartition_int(int arr[], int low, int high) 
+{
+    int pivot = arr[low]; 
+    int i = low; 
+    int j = high; 
+  
+    while (i < j)
+    { 
+  
+        // condition 1: find the first element greater than 
+        // the pivot (from starting) 
+        while (arr[i] <= pivot && i <= high - 1)
+        { 
+            i++; 
+        } 
+  
+        // condition 2: find the first element smaller than 
+        // the pivot (from last) 
+        while (arr[j] > pivot && j >= low + 1)
+        { 
+            j--; 
+        } 
+
+        if (i < j)
+        { 
+            swapMem(&arr[i], &arr[j]); 
+        } 
+    } 
+    swapMem(&arr[low], &arr[j]);
+
+    return j;
+}
+
+int quickSort_int(int arr[], int low, int high) 
+{ 
+    if (low < high)
+    { 
+
+        int partitionIndex = quickSortPartition_int(arr, low, high);
+  
+        // Recursively call quickSort_int() for left and right 
+        // half based on partition Index 
+        quickSort_int(arr, low, partitionIndex - 1); 
+        quickSort_int(arr, partitionIndex + 1, high); 
+    } 
+
+
+    return 0;
+} 
+
+int gcd(int element1, int element2)
+{
+    int a;
+    int b;
+    int remainder;
+
+    //making the 'a' variable the larger value
+    if(element1 > element2)
+    {
+        a = element1;
+        b = element2;
+    }
+    else
+    {
+        a = element2;
+        b = element1;
+    }
+
+    //using euclids gcd method
+    remainder = a - b;
+
+    if(a % remainder || b % remainder)
+    {
+        return gcd(b, remainder);
+    }
+    else
+    {
+        return remainder;
+    }
+
+
+}
+
+
